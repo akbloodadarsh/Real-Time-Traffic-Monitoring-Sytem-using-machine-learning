d4ba676dbb23fd263a5d2d9bb206a01c9ff78e70
Adarsh Kunwar <adarsh.infinite@gmail.com>
2020-01-04 17:25:00 +0530

    Update VehicleDetect.py

--- VehicleDetect.py
+++ VehicleDetect.py
@@ -1,5 +1,6 @@
 import cv2 as cv  # Importing computer vision as cv
 import numpy as np  # Importing numpy for array processing
+from tkinter import *
 
 confThreshold = 0.25
 '''Confidence threshold:-Confidence is a numerical value that is assigned to each Label while Predict is evaluating an
@@ -112,10 +113,8 @@ def postprocess(frame, p_frame, v_frame, outs, prev_veh_count, prev_ped_count):
         v_frame, p_frame = drawPred(classIDs[i], left, top, left + width, top + height, v_frame, p_frame)
     # if previous count is different than current count then print & store count in previous count
     if prev_veh_count != count:
-        print("Number of vehicles=", count)
         prev_veh_count = count
     if prev_ped_count != p_count:
-        print("Number of pedestrains=", p_count)
         prev_ped_count = p_count
     return prev_veh_count, prev_ped_count, p_frame, v_frame
 
@@ -178,122 +177,153 @@ def filtering(fil_frame):
     return fil_frame
 
 
-# *****MAIN***** #
-print("Press 1 to save the video\nPress any other key to continue without saving\n")
-choice = str(input())
+def execute():
+    # *****MAIN***** #
+    file = str(Entry1.get())
+    choice = ch.get()
+    cam_feed = feed.get()
+    # read the weights and configuration and creates the network
+    net = cv.dnn.readNetFromDarknet(modelConf, modelWeights)
+    # Ask network to use specific computation backend where it supported
+    net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+    # Ask network to make computations on specific target device
+    net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
 
-# read the weights and configuration and creates the network
-net = cv.dnn.readNetFromDarknet(modelConf, modelWeights)
-# Ask network to use specific computation backend where it supported
-net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
-# Ask network to make computations on specific target device
-net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+    # To set the name of window
+    v_window = 'vehicles'
+    p_window = 'pedestrians'
+    # To open the frame which is bigger in size
+    cv.namedWindow(v_window, cv.WINDOW_NORMAL)
+    cv.namedWindow(p_window, cv.WINDOW_NORMAL)
+    # To set size of video o/p window
+    cv.resizeWindow(v_window, 700, 450)
+    cv.resizeWindow(p_window, 700, 450)
 
-# To set the name of window
-v_window = 'vehicles'
-p_window = 'pedestrians'
-# To open the frame which is bigger in size
-cv.namedWindow(v_window, cv.WINDOW_NORMAL)
-cv.namedWindow(p_window, cv.WINDOW_NORMAL)
-# To set size of video o/p window
-cv.resizeWindow(v_window, 700, 450)
-cv.resizeWindow(p_window, 700, 450)
+    # To import video
+    cap = cv.VideoCapture(file)
+    if cap.isOpened() != True:
+        if cam_feed != 0:
+            cam_feed = 1
+            cap = cv.VideoCapture(cam_feed)
+            if cap.isOpened() != True:
+                cam_feed = 0
+                cap = cv.VideoCapture(cam_feed)
 
-# To import video
-cap = cv.VideoCapture('test1')
-v_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
-v_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+    v_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+    v_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 
-# For having a constant size in every resolution
-scale = 0.05
-fontScale = min(v_width, v_height)/(25/scale)
-font = cv.FONT_HERSHEY_DUPLEX
-# For having a constant size of rectangle in every resolution
-rec_width = int(v_width/1.8)
-rec_height = int(v_height/6.8)
+    # For having a constant size in every resolution
+    scale = 0.05
+    fontScale = min(v_width, v_height) / (25 / scale)
+    font = cv.FONT_HERSHEY_DUPLEX
+    # For having a constant size of rectangle in every resolution
+    rec_width = int(v_width / 1.8)
+    rec_height = int(v_height / 6.8)
 
-# setting the initial previous count
-# For vehicles
-prev_veh_count = 0
-# For pedestrians
-prev_ped_count = 0
+    # setting the initial previous count
+    # For vehicles
+    prev_veh_count = 0
+    # For pedestrians
+    prev_ped_count = 0
 
-if choice == '1':
-    # setting codec to encode or decode digital stream
-    four_cc = cv.VideoWriter_fourcc(*'XVID')
+    if choice == '1':
+        # setting codec to encode or decode digital stream
+        four_cc = cv.VideoWriter_fourcc(*'XVID')
 
-    # to save video
-    save_ped = cv.VideoWriter('vid_p.avi', four_cc, 15, (v_width, v_height))
-    save_veh = cv.VideoWriter('vid_v.avi', four_cc, 15, (v_width, v_height))
+        # to save video
+        save_ped = cv.VideoWriter('vid_p.avi', four_cc, 15, (v_width, v_height))
+        save_veh = cv.VideoWriter('vid_v.avi', four_cc, 15, (v_width, v_height))
 
-    while True:
-        # get frame from video
-        ret, frame = cap.read()
-        # applying filter
-        frame = filtering(frame)
-        p_frame = frame
-        v_frame = frame
-        if ret:
-            # Create a 4D blob from a frame cv.dnn.blobFromImage(image,scale_factor,size,mean, swapRB,crop,depth)
-            '''a blob is a group of connected pixels,neighbouring pixels'''
-            blob = cv.dnn.blobFromImage(frame, 1 / 255, (inpWidth, inpHeight), [0, 0, 0], 1, crop=False)
-            # Set the i/p blob
-            net.setInput(blob)
-            # outs:-Array that contains all info about objects detected,their position,confidence about the detection
-            # forward:- (blob already set)compute o/p of all the layers and returns the blob
-            outs = net.forward(getOutputsNames(net))
-            # send the frame, detected obj,previous count and return the updated previous count
-            prev_veh_count, prev_ped_count, p_frame, v_frame = postprocess(frame, p_frame, v_frame, outs, prev_veh_count
-                                                                           , prev_ped_count)
-            v_text = "Number of vehicles=" + str(prev_veh_count)
-            v_frame = cv.rectangle(v_frame, (0, 0), (rec_width, rec_height), (0, 0, 0), -1)
-            # 0.8
-            v_frame = cv.putText(v_frame, v_text, (10, 30), font, fontScale, (0, 0, 255), 0, cv.LINE_AA)
-            # show the frame
-            cv.imshow(v_window, v_frame)
-            save_veh.write(v_frame)
-            p_text = "Number of pedestrians=" + str(prev_ped_count)
-            p_frame = cv.rectangle(p_frame, (0, 0), (rec_width, rec_height), (0, 0, 0), -1)
-            p_frame = cv.putText(p_frame, p_text, (10, 30), font, fontScale, (0, 0, 255), 0, cv.LINE_AA)
-            save_ped.write(p_frame)
-            cv.imshow(p_window, p_frame)
-            if cv.waitKey(1) & 0XFF == ord('q'):
-                break
-    save_ped.release()
-    save_veh.release()
-else:
-    while True:
-        # get frame from video
-        ret, frame = cap.read()
-        frame = filtering(frame)
-        p_frame = frame
-        v_frame = frame
-        if ret:
-            # Create a 4D blob from a frame cv.dnn.blobFromImage(image,scale_factor,size,mean, swapRB,crop,depth)
-            '''a blob is a group of connected pixels,neighbouring pixels'''
-            blob = cv.dnn.blobFromImage(frame, 1 / 255, (inpWidth, inpHeight), [0, 0, 0], 1, crop=False)
-            # Set the i/p blob
-            net.setInput(blob)
-            # outs:-Array that contains all info about objects detected,their position,confidence about the detection
-            # forward:- (blob already set)compute o/p of all the layers and returns the blob
-            outs = net.forward(getOutputsNames(net))
-            # send the frame, detected obj,previous count and return the updated previous count
-            prev_veh_count, prev_ped_count, p_frame, v_frame = postprocess(frame, p_frame, v_frame, outs, prev_veh_count
-                                                                           , prev_ped_count)
+        while True:
+            # get frame from video
+            ret, frame = cap.read()
+            # applying filter
+            frame = filtering(frame)
+            p_frame = frame
+            v_frame = frame
+            if ret:
+                # Create a 4D blob from a frame cv.dnn.blobFromImage(image,scale_factor,size,mean, swapRB,crop,depth)
+                '''a blob is a group of connected pixels,neighbouring pixels'''
+                blob = cv.dnn.blobFromImage(frame, 1 / 255, (inpWidth, inpHeight), [0, 0, 0], 1, crop=False)
+                # Set the i/p blob
+                net.setInput(blob)
+                # outs:-Array that contains all info about objects detected,their position,confidence about the detection
+                # forward:- (blob already set)compute o/p of all the layers and returns the blob
+                outs = net.forward(getOutputsNames(net))
+                # send the frame, detected obj,previous count and return the updated previous count
+                prev_veh_count, prev_ped_count, p_frame, v_frame = postprocess(frame, p_frame, v_frame, outs,
+                                                                               prev_veh_count
+                                                                               , prev_ped_count)
+                v_text = "Number of vehicles=" + str(prev_veh_count)
+                v_frame = cv.rectangle(v_frame, (0, 0), (rec_width, rec_height), (0, 0, 0), -1)
+                # 0.8
+                v_frame = cv.putText(v_frame, v_text, (10, 30), font, fontScale, (0, 0, 255), 0, cv.LINE_AA)
+                # show the frame
+                cv.imshow(v_window, v_frame)
+                save_veh.write(v_frame)
+                p_text = "Number of pedestrians=" + str(prev_ped_count)
+                p_frame = cv.rectangle(p_frame, (0, 0), (rec_width, rec_height), (0, 0, 0), -1)
+                p_frame = cv.putText(p_frame, p_text, (10, 30), font, fontScale, (0, 0, 255), 0, cv.LINE_AA)
+                save_ped.write(p_frame)
+                cv.imshow(p_window, p_frame)
+                if cv.waitKey(1) & 0XFF == ord('q'):
+                    break
+        save_ped.release()
+        save_veh.release()
+    else:
+        while True:
+            # get frame from video
+            ret, frame = cap.read()
+            frame = filtering(frame)
+            p_frame = frame
+            v_frame = frame
+            if ret:
+                # Create a 4D blob from a frame cv.dnn.blobFromImage(image,scale_factor,size,mean, swapRB,crop,depth)
+                '''a blob is a group of connected pixels,neighbouring pixels'''
+                blob = cv.dnn.blobFromImage(frame, 1 / 255, (inpWidth, inpHeight), [0, 0, 0], 1, crop=False)
+                # Set the i/p blob
+                net.setInput(blob)
+                # outs:-Array contains all info about objects detected,their position,confidence about the detection
+                # forward:- (blob already set)compute o/p of all the layers and returns the blob
+                outs = net.forward(getOutputsNames(net))
+                # send the frame, detected obj,previous count and return the updated previous count
+                prev_veh_count, prev_ped_count, p_frame, v_frame = postprocess(frame, p_frame, v_frame, outs,
+                                                                               prev_veh_count
+                                                                               , prev_ped_count)
 
-            # For vehicles
-            v_text = "Number of vehicles=" + str(prev_veh_count)
-            v_frame = cv.rectangle(v_frame, (0, 0), (rec_width, rec_height), (0, 0, 0), -1)
-            v_frame = cv.putText(v_frame, v_text, (10, 30), font, fontScale, (255, 255, 255), 0)
-            # show the frame
-            cv.imshow(v_window, v_frame)
+                # For vehicles
+                v_text = "Number of vehicles=" + str(prev_veh_count)
+                v_frame = cv.rectangle(v_frame, (0, 0), (rec_width, rec_height), (0, 0, 0), -1)
+                v_frame = cv.putText(v_frame, v_text, (10, 30), font, fontScale, (255, 255, 255), 0)
+                # show the frame
+                cv.imshow(v_window, v_frame)
 
-            # For pedestrians
-            p_text = "Number of pedestrians=" + str(prev_ped_count)
-            p_frame = cv.rectangle(p_frame, (0, 0), (rec_width, rec_height), (0, 0, 0), -1)
-            p_frame = cv.putText(p_frame, p_text, (10, 30), font, fontScale, (255, 255, 255), 0)
-            # show the frame
-            cv.imshow(p_window, p_frame)
-            if cv.waitKey(1) & 0XFF == ord('q'):
-                break
-cv.destroyAllWindows()
+                # For pedestrians
+                p_text = "Number of pedestrians=" + str(prev_ped_count)
+                p_frame = cv.rectangle(p_frame, (0, 0), (rec_width, rec_height), (0, 0, 0), -1)
+                p_frame = cv.putText(p_frame, p_text, (10, 30), font, fontScale, (255, 255, 255), 0)
+                # show the frame
+                cv.imshow(p_window, p_frame)
+                if cv.waitKey(1) & 0XFF == ord('q'):
+                    break
+    cv.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    mainwindow = Tk()
+    mainwindow.title("Vehicle & Pedestrians D&C")
+    Label(mainwindow, text="Vehicle & Pedestrians D&C", bg="black", fg="white").pack(side=TOP, fill=X, padx=2, pady=2)
+    photo = PhotoImage(file="download.png")
+    Label(mainwindow, image=photo, bg="black", fg="white").pack(side=TOP, fill=X)
+    Label(mainwindow, text="File Name", bg="black", fg="white").pack(side=TOP, fill=X, padx=2, pady=2)
+    Entry1 = Entry(mainwindow)
+    Entry1.pack(side=TOP, padx=2, pady=2)
+    Label(mainwindow, text="Slide to save", bg="black", fg="white").pack(side=TOP, fill=X, padx=2, pady=2)
+    ch = Scale(mainwindow, from_=0, to=1, orient=HORIZONTAL)
+    ch.pack(side=TOP, fill=X, padx=2, pady=2)
+    Label(mainwindow, text="Slide to use camera", bg="black", fg="white").pack(side=TOP, fill=X, padx=2, pady=2)
+    feed = Scale(mainwindow, from_=0, to=1, orient=HORIZONTAL)
+    feed.pack(side=TOP, fill=X, padx=2, pady=2)
+    But1 = Button(mainwindow, text="RUN", command=execute)
+    But1.pack(side=TOP, fill=X, padx=2, pady=2)
+    mainwindow.mainloop()
